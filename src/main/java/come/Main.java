7aec64c92995bac9a1ab65c8e007366b589e2098
SkankHunt3r42 <nikitos77@i.ua>
2024-04-11 15:38:26 +0300

    Add passowr

--- src/main/java/come/Main.java
+++ src/main/java/come/Main.java
@@ -1,5 +1,32 @@
+package main.java.come;
+
+import java.util.Scanner;
+
 public class Main {
+
+    static  String passwor = "default";
+    static Scanner scanner = new Scanner(System.in);
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        run();
     }
+    static  void run() {
+        boolean accepted = false;
+        for(int a = 0;a<3;a++) {
+            System.out.println("Password ");
+            String input = scanner.nextLine();
+
+            if(passwor.equals(input)) {
+                accepted = true;
+                break;
+            } else {
+                System.out.println("Accept denied. Please check your password.");
+            }
+
+        }
+        System.out.println(accepted ? "Welcome to Magic Vet!" : "Aplication has been blocked.");
+    }
+
 }
