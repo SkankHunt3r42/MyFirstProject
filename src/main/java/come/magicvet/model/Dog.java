00f1b0470b9bc3f61dee663f015750a605c3a34a
SkankHunt3r42 <nikitos77@i.ua>
2024-05-07 22:22:06 +0300

    REQ-06_SortDog_by_Age&&Size

--- src/main/java/come/magicvet/model/Dog.java
+++ src/main/java/come/magicvet/model/Dog.java
@@ -3,7 +3,20 @@ package main.java.come.magicvet.model;
 
 public class Dog extends  Pet{
 
+    public static final String XS = "XS";
+    public static final String S = "S";
+    public static final String M = "M";
+    public static final String L  = "L";
+    public static final String XL = "XL";
+
     private  String size;
+
+    public Dog(){}
+
+    public Dog(String size,String age){
+        super.setAge(age);
+        this.size = size;
+    }
 
     public void setSize(String size) {
         this.size = size;
